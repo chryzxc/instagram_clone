d9d2e86127ae19c1f3b10701a64d730eaaa83eee
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2022-02-16 19:18:48 +0800

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -21,20 +19,56 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     body: SafeArea(
-        child: Center(
-          child: Text(
-            "Instagram",
-            style: TextStyle(fontSize: 25),
-            textAlign: TextAlign.center,
+      body: Container(
+        margin: const EdgeInsets.fromLTRB(20,10,20,0),
+        child: SafeArea(
+          child: Column(
+            children: <Widget> [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(
+                    Icons.camera_alt,
+                    color: Colors.grey[900],
+                    size: 30.0,
+                  ),
+                  const Text(
+                    "Instagram",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Icon(
+                    Icons.send,
+                    color: Colors.grey[900],
+                    size: 30.0,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage('https://via.placeholder.com/140x100'),
+
+                  ),
+                  CircleAvatar(
+
+                  ),
+                  CircleAvatar(
+
+                  ),
+                ],
+              ),
+            ],
           ),
-        )
-       ,
-     ),
+        ),
+      ),
 
     );
-
   }
 }
-
-
